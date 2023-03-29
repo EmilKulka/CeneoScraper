13b54b66056da36752dcd8b11b775c05a3c60b56
EmilKulka <226310@student.uek.krakow.pl>
2023-03-29 14:38:12 +0200

    2023-03-29 EK

---
+++ scraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+# product_code = input("Podaj kod produktu: ")
+product_code = "123849599"
+# url = "https://www.ceneo.pl/"+ product_code +"#tab=reviews"
+url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+response = requests.get(url)
+page_dom = BeautifulSoup(response.text, "html.parser")
+opinions = page_dom.select("div.js_product-review")
+for opinion in opinions:
+    print(opinion["data-entry-id"])
